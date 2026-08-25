2f0c5dbe7bd38ce9103d23dd10ab7a523c98233f
Duncan Macleod <macleoddm@cardiff.ac.uk>
2026-08-25 19:23:11 +0100

    chore: housekeeping

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+set -ex
+
 mkdir -p _build
 pushd _build
 
 # configure
-cmake ${SRC_DIR} \
-	${CMAKE_ARGS} \
-	-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
-	-DCMAKE_INSTALL_DATADIR:PATH=${SRC_DIR}/trash \
-	-DCMAKE_INSTALL_LIBDIR:PATH="lib" \
-	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-	-DPython3_EXECUTABLE:FILE=${PYTHON} \
-;
+cmake \
+  ${CMAKE_ARGS} \
+  -DCMAKE_INSTALL_DATADIR:PATH=${SRC_DIR}/trash \
+  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+  -DPython3_EXECUTABLE:FILE=${PYTHON} \
+  ${SRC_DIR}
 
 # build
 cmake --build . --parallel ${CPU_COUNT}
@@ -20,4 +20,4 @@ cmake --build . --parallel ${CPU_COUNT}
 cmake --build . --parallel ${CPU_COUNT} --target install
 
 # test
-ctest
+ctest --output-on-failure --parallel ${CPU_COUNT} --verbose
